44486997bb57b24da581309f9d0faae3fd7ad98f
technoscope <45495103+technoscope@users.noreply.github.com>
2020-11-04 20:10:32 +0500

    ahmad commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Only Changes by Ahmad22 '),
+      home: MyHomePage(title: 'Flutter Only Changes by nottiiii '),
     );
   }
 }
