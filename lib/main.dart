7bc337e91f8d2998336287d3aca6f1a619b32895
Saif Qureshi <48083045+safu2011@users.noreply.github.com>
2020-11-04 20:19:58 +0500

    Merge branch 'master' of https://github.com/technoscope/wombatapp
     Conflicts:
    	lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Only Changes by kusdasdasd '),
+      home: MyHomePage(title: 'Flutter Only Changes by kusdasxcvsdczxcdsvdfsbdasd '),
     );
   }
 }
