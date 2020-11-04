daff1931ed314b73b44e757e3b2d98320d6c674c
technoscope <45495103+technoscope@users.noreply.github.com>
2020-11-04 20:16:49 +0500

    ahmad commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Only Changes by nottiiii '),
+      home: MyHomePage(title: 'Flutter Only Changes by ku '),
     );
   }
 }
